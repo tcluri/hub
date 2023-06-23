10da848cbed395b5b788365c59c942a6e6564ede
Tejaa Chintaluri <saitejaac@gmail.com>
2023-06-23 21:45:14 +0530

    Add proper forms, views and respective templates

--- server/forms.py
+++ server/forms.py
@@ -1,39 +1,76 @@
 from django import forms
-from .models import User, Player, Guardian, Membership, Vaccination
+from .models import User, Event, Player, Guardian, Membership, Vaccination
 
+GENDER_CHOICES = [('male', 'Male'), ('female', 'Female'), ('other', 'Other')]
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'phone']
+        fields = ['is_player', 'is_guardian', 'email', 'phone']
 
 
 class PlayerForm(forms.ModelForm):
+    date_of_birth = forms.DateField(widget=forms.DateInput(attrs={'type' : 'date'}))
+    gender = forms.ChoiceField(choices=GENDER_CHOICES)
+    other_gender = forms.CharField(required=False, max_length=20)
+
     class Meta:
         model = Player
-        fields = ['first_name', 'last_name', 'date_of_birth', 'gender', 'city', 'state_ut', 'team_name',
-                  'occupation', 'educational_institution', 'india_ultimate_profile', 'guardian']
+        fields = ['first_name', 'last_name', 'date_of_birth', 'gender', 'other_gender','city', 'state_ut', 'team_name',
+                  'occupation', 'educational_institution', 'india_ultimate_profile']
+        labels = {
+            'state_ut' : 'State/UT',
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        gender = cleaned_data.get('gender')
+        other_gender = cleaned_data.get('other_gender')
+
+        if gender == 'other' and not other_gender:
+            self.add_error('other_gender', 'Please specify the gender.')
+
+        return cleaned_data
 
 
 class GuardianForm(forms.ModelForm):
     class Meta:
         model = Guardian
         fields = ['full_name', 'relation']
+        labels = {
+            'full_name' : 'Guardian\'s Fullname',
+            'relation' : 'Guardian\'s relation to the minor'
+        }
 
 
 class EventForm(forms.ModelForm):
+    start_date = forms.DateField(widget=forms.DateInput(attrs={'type' : 'date'}))
+    end_date = forms.DateField(widget=forms.DateInput(attrs={'type' : 'date'}))
     class Meta:
         model = Event
         fields = ['title', 'start_date', 'end_date']
+        labels = {
+            'title' : 'Event title',
+            'start_date': 'Event start date',
+            'end_date': 'Event end date',
+        }
 
 
 class MembershipForm(forms.ModelForm):
+    start_date = forms.DateField(widget=forms.DateInput(attrs={'type' : 'date'}))
+    end_date = forms.DateField(widget=forms.DateInput(attrs={'type' : 'date'}))
     class Meta:
         model = Membership
-        fields = ['player', 'is_annual', 'start_date', 'end_date', 'event', 'is_active']
+        fields = ['membership_number', 'is_annual', 'start_date', 'end_date', 'is_active']
+        labels = {
+            'is_annual' : 'Is Annual membership',
+            'start_date' : 'Membership start date',
+            'end_date' : 'Membership end date',
+            'is_active' : 'Membership currently active'
+        }
 
 
 class VaccinationForm(forms.ModelForm):
     class Meta:
         model = Vaccination
-        fields = ['player', 'is_vaccinated', 'vaccination_name', 'vaccination_certificate', 'explain_not_vaccinated']
+        fields = ['is_vaccinated', 'vaccination_name', 'vaccination_certificate', 'explain_not_vaccinated']
--- server/views.py
+++ server/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserForm, PlayerForm, GuardianForm, EventForm, MembershipForm, VaccinationForm
+from .models import User
 
 # Create your views here.
+def home(request):
+    return render(request, 'base.html')
+
+def registration_view(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        player_form = PlayerForm(request.POST)
+        guardian_form = GuardianForm(request.POST)
+        event_form = EventForm(request.POST)
+        membership_form = MembershipForm(request.POST)
+        vaccination_form = VaccinationForm(request.POST)
+
+        if user_form.is_valid() and player_form.is_valid() and event_form.is_valid() and membership_form.is_valid() and vaccination_form.is_valid():
+            user = user_form.save(commit=False)
+            player = player_form.save(commit=False)
+            guardian = guardian_form.save(commit=False)
+            event = event_form.save(commit=False)
+            membership = membership_form.save(commit=False)
+            vaccination = vaccination_form.save(commit=False)
+
+            return redirect('success')
+
+    else:
+        user_form = UserForm()
+        player_form = PlayerForm()
+        guardian_form = GuardianForm()
+        event_form = EventForm()
+        membership_form = MembershipForm()
+        vaccination_form = VaccinationForm()
+
+    context = {
+        'user_form': user_form,
+        'player_form': player_form,
+        'guardian_form': guardian_form,
+        'event_form': event_form,
+        'membership_form': membership_form,
+        'vaccination_form': vaccination_form
+    }
+
+    return render(request, 'registration.html', context)
+
+
+def success_view(request):
+    return render(request, 'success.html')
